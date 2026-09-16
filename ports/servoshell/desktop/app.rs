servo/servo

servoshell: Have `window.open()` in servoshell in new windows
Signed-off-by: Martin Robinson <mrobinson@igalia.com>

ports/servoshell/desktop/app.rs
@@ -30,7 +30,7 @@ use crate::prefs::ServoShellPreferences;
 use crate::running_app_state::RunningAppState;
 #[cfg(feature = "gamepad")]
 use crate::running_app_state::ServoshellGamepadDelegate;
-use crate::window::{PlatformWindow, ServoShellWindowId};
+use crate::window::{PlatformWindow, ServoShellWindowId, TopLevelWebViewCreationType};
 
 pub(crate) enum AppState {
     Initializing,
@@ -132,7 +132,11 @@ impl App {
             #[cfg(feature = "gamepad")]
             ServoshellGamepadDelegate::maybe_new().map(Rc::new),
         ));
-        running_state.open_window(platform_window, self.initial_url.as_url().clone());
+        let initial_url = self.initial_url.as_url().clone();
+        running_state.open_window(
+            platform_window,
+            TopLevelWebViewCreationType::WithUrl(initial_url),
+        );
 
         self.state = AppState::Running(running_state);
     }

ports/servoshell/desktop/gui.rs
@@ -37,7 +37,7 @@ use winit::window::Window;
 use crate::desktop::event_loop::AppEvent;
 use crate::desktop::headed_window;
 use crate::running_app_state::{RunningAppState, UserInterfaceCommand};
-use crate::window::ServoShellWindow;
+use crate::window::{ServoShellWindow, TopLevelWebViewCreationType};
 
 /// The user interface of a headed servoshell. Currently this is implemented via
 /// egui.
@@ -564,8 +564,9 @@ impl Gui {
                                         info
                                     });
                                     if new_window_button.clicked() {
+                                        let url = Url::parse("servo:newtab").expect("Should be able to unconditionally parse 'servo:newtab' as URL");
                                         window.queue_user_interface_command(
-                                            UserInterfaceCommand::NewWindow,
+                                            UserInterfaceCommand::NewWindow(TopLevelWebViewCreationType::WithUrl(url)),
                                         );
                                     }
                                 },

ports/servoshell/desktop/headed_window.rs
@@ -56,7 +56,7 @@ use crate::prefs::ServoShellPreferences;
 use crate::running_app_state::{RunningAppState, UserInterfaceCommand};
 use crate::window::{
     LINE_HEIGHT, LINE_WIDTH, MIN_WINDOW_INNER_SIZE, PlatformWindow, ServoShellWindow,
-    ServoShellWindowId,
+    ServoShellWindowId, TopLevelWebViewCreationType,
 };
 
 pub(crate) const INITIAL_WINDOW_TITLE: &str = "Servo";
@@ -437,10 +437,11 @@ impl HeadedWindow {
                 }
             })
             .shortcut(CMD_OR_CONTROL, 'T', || {
+                let url = Url::parse("servo:newtab")
+                    .expect("Should be able to unconditionally parse 'servo:newtab' as URL");
                 window.create_and_activate_toplevel_webview(
                     state.clone(),
-                    Url::parse("servo:newtab")
-                        .expect("Should be able to unconditionally parse 'servo:newtab' as URL"),
+                    TopLevelWebViewCreationType::WithUrl(url),
                 );
             })
             .shortcut(CMD_OR_CONTROL, 'Q', || state.schedule_exit())

ports/servoshell/running_app_state.rs
@@ -36,7 +36,9 @@ use url::Url;
 pub(crate) use crate::desktop::gamepad::ServoshellGamepadDelegate;
 use crate::prefs::{EXPERIMENTAL_PREFS, ServoShellPreferences};
 use crate::webdriver::WebDriverEmbedderControls;
-use crate::window::{PlatformWindow, ServoShellWindow, ServoShellWindowId};
+use crate::window::{
+    PlatformWindow, ServoShellWindow, ServoShellWindowId, TopLevelWebViewCreationType,
+};
 
 #[cfg(all(
     any(coverage, llvm_pgo),
@@ -150,7 +152,6 @@ impl WebViewCollection {
 }
 
 /// A command received via the user interacting with the user interface.
-#[cfg_attr(any(target_os = "android", target_env = "ohos"), expect(dead_code))]
 pub(crate) enum UserInterfaceCommand {
     Go(String),
     Back,
@@ -159,7 +160,7 @@ pub(crate) enum UserInterfaceCommand {
     ReloadAll,
     NewWebView,
     CloseWebView(WebViewId),
-    NewWindow,
+    NewWindow(TopLevelWebViewCreationType),
 }
 
 pub(crate) struct RunningAppState {
@@ -279,13 +280,13 @@ impl RunningAppState {
     pub(crate) fn open_window(
         self: &Rc<Self>,
         platform_window: Rc<dyn PlatformWindow>,
-        initial_url: Url,
+        creation_request: TopLevelWebViewCreationType,
     ) -> Rc<ServoShellWindow> {
         let window = Rc::new(ServoShellWindow::new(platform_window.clone()));
         self.windows
             .borrow_mut()
             .insert(window.id(), window.clone());
-        window.create_and_activate_toplevel_webview(self.clone(), initial_url);
+        window.create_and_activate_toplevel_webview(self.clone(), creation_request);
 
         // If the window already has platform focus, mark it as focused in our application state.
         if platform_window.has_platform_focus() {
@@ -411,12 +412,6 @@ impl RunningAppState {
         self: &Rc<Self>,
         create_platform_window: Option<&dyn Fn(Url) -> Rc<dyn PlatformWindow>>,
     ) -> bool {
-        // We clone here to avoid a double borrow. User interface commands can update the list of windows.
-        let windows: Vec<_> = self.windows.borrow().values().cloned().collect();
-        for window in windows {
-            window.handle_interface_commands(self, create_platform_window);
-        }
-
         self.handle_webdriver_messages(create_platform_window);
 
         #[cfg(all(
@@ -431,6 +426,12 @@ impl RunningAppState {
 
         for window in self.windows.borrow().values() {
             window.update_and_request_repaint_if_necessary(self);
+        }
+
+        // We clone here to avoid a double borrow. User interface commands can update the list of windows.
+        let windows: Vec<_> = self.windows.borrow().values().cloned().collect();
+        for window in windows {
+            window.handle_interface_commands(self, create_platform_window);
         }
 
         if self.servoshell_preferences.exit_after_stable_image && self.achieved_stable_image.get() {
@@ -741,6 +742,17 @@ impl WebViewDelegate for RunningAppState {
 
     fn request_create_new(&self, parent_webview: WebView, request: CreateNewWebViewRequest) {
         let window = self.window_for_webview_id(parent_webview.id());
+
+        // When WebDriver wants to open a new WebView, do this in a new window. Normally, servoshell
+        // throttles WebViews that are hidden, but WebDriver expects all opened WebViews to stay
+        // active. Using a separate window ensures this.
+        if self.servoshell_preferences.webdriver_port.get().is_some() {
+            window.queue_user_interface_command(UserInterfaceCommand::NewWindow(
+                TopLevelWebViewCreationType::WithCreateRequest(request),
+            ));
+            return;
+        }
+
         let platform_window = window.platform_window();
         let webview = request
             .builder(platform_window.rendering_context())
@@ -750,15 +762,7 @@ impl WebViewDelegate for RunningAppState {
 
         webview.notify_theme_change(platform_window.theme());
         window.add_webview(webview.clone());
-
-        // When WebDriver is enabled, do not focus and raise the WebView to the top,
-        // as that is what the specification expects. Otherwise, we would like `window.open()`
-        // to create a new foreground tab
-        if self.servoshell_preferences.webdriver_port.get().is_none() {
-            window.activate_webview(webview.id());
-        } else {
-            webview.hide();
-        }
+        window.activate_webview(webview.id());
     }
 
     fn notify_closed(&self, webview: WebView) {

ports/servoshell/webdriver.rs
@@ -14,7 +14,7 @@ use servo::{
 use url::Url;
 
 use crate::running_app_state::RunningAppState;
-use crate::window::PlatformWindow;
+use crate::window::{PlatformWindow, TopLevelWebViewCreationType};
 
 #[derive(Default)]
 pub(crate) struct WebDriverEmbedderControls {
@@ -162,7 +162,10 @@ impl RunningAppState {
                             NewWindowTypeHint::Window | NewWindowTypeHint::Auto,
                             Some(create_platform_window),
                         ) => {
-                            let window = self.open_window(create_platform_window(url.clone()), url);
+                            let window = self.open_window(
+                                create_platform_window(url.clone()),
+                                TopLevelWebViewCreationType::WithUrl(url),
+                            );
                             window
                                 .active_webview()
                                 .expect("Should have at last one WebView in new window")
@@ -172,7 +175,10 @@ impl RunningAppState {
                             .values()
                             .nth(0)
                             .expect("Expected at least one window to be open")
-                            .create_toplevel_webview(self.clone(), url),
+                            .create_toplevel_webview(
+                                self.clone(),
+                                TopLevelWebViewCreationType::WithUrl(url),
+                            ),
                     };
 
                     if let Err(error) = response_sender.send(new_webview.id()) {

legatus-ai/servo-engine

ports/servoshell/desktop/app.rs
@@ -30,7 +30,7 @@ use crate::prefs::ServoShellPreferences;
 use crate::running_app_state::RunningAppState;
 #[cfg(feature = "gamepad")]
 use crate::running_app_state::ServoshellGamepadDelegate;
-use crate::window::{PlatformWindow, ServoShellWindowId};
+use crate::window::{PlatformWindow, ServoShellWindowId, TopLevelWebViewCreationRequest};
 
 pub(crate) enum AppState {
     Initializing,
@@ -135,7 +135,11 @@ impl App {
                 .map(ServoshellGamepadDelegate::new)
                 .map(Rc::new),
         ));
-        running_state.open_window(platform_window, self.initial_url.as_url().clone());
+        let initial_url = self.initial_url.as_url().clone();
+        running_state.open_window(
+            platform_window,
+            TopLevelWebViewCreationRequest::WithUrl(initial_url),
+        );
 
         self.state = AppState::Running(running_state);
     }

ports/servoshell/desktop/gui.rs
@@ -58,7 +58,7 @@ use winit::window::Window;
 use crate::desktop::event_loop::AppEvent;
 use crate::desktop::headed_window;
 use crate::running_app_state::{RunningAppState, UserInterfaceCommand};
-use crate::window::ServoShellWindow;
+use crate::window::{ServoShellWindow, TopLevelWebViewCreationRequest};
 
 /// The user interface of a headed servoshell. Currently this is implemented via
 /// egui.
@@ -595,8 +595,9 @@ impl Gui {
                                         info
                                     });
                                     if new_window_button.clicked() {
+                                        let url = Url::parse("servo:newtab").expect("Should be able to unconditionally parse 'servo:newtab' as URL");
                                         window.queue_user_interface_command(
-                                            UserInterfaceCommand::NewWindow,
+                                            UserInterfaceCommand::NewWindow(TopLevelWebViewCreationRequest::WithUrl(url)),
                                         );
                                     }
                                 },

ports/servoshell/desktop/headed_window.rs
@@ -54,7 +54,7 @@ use crate::prefs::ServoShellPreferences;
 use crate::running_app_state::{RunningAppState, UserInterfaceCommand};
 use crate::window::{
     LINE_HEIGHT, LINE_WIDTH, MIN_WINDOW_INNER_SIZE, PlatformWindow, ServoShellWindow,
-    ServoShellWindowId,
+    ServoShellWindowId, TopLevelWebViewCreationRequest,
 };
 
 pub(crate) const INITIAL_WINDOW_TITLE: &str = "Servo";
@@ -423,10 +423,11 @@ impl HeadedWindow {
                 }
             })
             .shortcut(CMD_OR_CONTROL, 'T', || {
+                let url = Url::parse("servo:newtab")
+                    .expect("Should be able to unconditionally parse 'servo:newtab' as URL");
                 window.create_and_activate_toplevel_webview(
                     state.clone(),
-                    Url::parse("servo:newtab")
-                        .expect("Should be able to unconditionally parse 'servo:newtab' as URL"),
+                    TopLevelWebViewCreationRequest::WithUrl(url),
                 );
             })
             .shortcut(CMD_OR_CONTROL, 'Q', || state.schedule_exit())

ports/servoshell/running_app_state.rs
@@ -46,7 +46,9 @@ use url::Url;
 pub(crate) use crate::desktop::gamepad::ServoshellGamepadDelegate;
 use crate::prefs::{EXPERIMENTAL_PREFS, ServoShellPreferences};
 use crate::webdriver::WebDriverEmbedderControls;
-use crate::window::{PlatformWindow, ServoShellWindow, ServoShellWindowId};
+use crate::window::{
+    PlatformWindow, ServoShellWindow, ServoShellWindowId, TopLevelWebViewCreationRequest,
+};
 
 #[cfg(all(
     any(coverage, llvm_pgo),
@@ -165,7 +167,7 @@ pub(crate) enum UserInterfaceCommand {
     ReloadAll,
     NewWebView,
     CloseWebView(WebViewId),
-    NewWindow,
+    NewWindow(TopLevelWebViewCreationRequest),
 }
 
 pub(crate) struct RunningAppState {
@@ -285,13 +287,13 @@ impl RunningAppState {
     pub(crate) fn open_window(
         self: &Rc<Self>,
         platform_window: Rc<dyn PlatformWindow>,
-        initial_url: Url,
+        creation_request: TopLevelWebViewCreationRequest,
     ) -> Rc<ServoShellWindow> {
         let window = Rc::new(ServoShellWindow::new(platform_window.clone()));
         self.windows
             .borrow_mut()
             .insert(window.id(), window.clone());
-        window.create_and_activate_toplevel_webview(self.clone(), initial_url);
+        window.create_and_activate_toplevel_webview(self.clone(), creation_request);
 
         // If the window already has platform focus, mark it as focused in our application state.
         if platform_window.has_platform_focus() {
@@ -418,12 +420,6 @@ impl RunningAppState {
         self: &Rc<Self>,
         create_platform_window: Option<&dyn Fn(Url) -> Rc<dyn PlatformWindow>>,
     ) -> bool {
-        // We clone here to avoid a double borrow. User interface commands can update the list of windows.
-        let windows: Vec<_> = self.windows.borrow().values().cloned().collect();
-        for window in windows {
-            window.handle_interface_commands(self, create_platform_window);
-        }
-
         self.handle_webdriver_messages(create_platform_window);
 
         /* #[cfg(all(
@@ -438,6 +434,12 @@ impl RunningAppState {
 
         for window in self.windows.borrow().values() {
             window.update_and_request_repaint_if_necessary(self);
+        }
+
+        // We clone here to avoid a double borrow. User interface commands can update the list of windows.
+        let windows: Vec<_> = self.windows.borrow().values().cloned().collect();
+        for window in windows {
+            window.handle_interface_commands(self, create_platform_window);
         }
 
         if self.servoshell_preferences.exit_after_stable_image && self.achieved_stable_image.get() {
@@ -751,8 +753,18 @@ impl WebViewDelegate for RunningAppState {
 
     fn request_create_new(&self, parent_webview: WebView, request: CreateNewWebViewRequest) {
         let window = self.window_for_webview(&parent_webview);
-        let platform_window = window.platform_window();
 
+        // When WebDriver wants to open a new WebView, do this in a new window. Normally, servoshell
+        // throttles WebViews that are hidden, but WebDriver expects all opened WebViews to stay
+        // active. Using a separate window ensures this.
+        if self.servoshell_preferences.webdriver_port.get().is_some() {
+            window.queue_user_interface_command(UserInterfaceCommand::NewWindow(
+                TopLevelWebViewCreationRequest::WithCreateRequest(request),
+            ));
+            return;
+        }
+
+        let platform_window = window.platform_window();
         let webview = request
             .builder(platform_window.rendering_context())
             .hidpi_scale_factor(platform_window.hidpi_scale_factor())
@@ -761,15 +773,7 @@ impl WebViewDelegate for RunningAppState {
 
         webview.notify_theme_change(platform_window.theme());
         window.add_webview(webview.clone());
-
-        // When WebDriver is enabled, do not focus and raise the WebView to the top,
-        // as that is what the specification expects. Otherwise, we would like `window.open()`
-        // to create a new foreground tab
-        if self.servoshell_preferences.webdriver_port.get().is_none() {
-            window.activate_webview(webview.id());
-        } else {
-            webview.hide();
-        }
+        window.activate_webview(webview.id());
     }
 
     fn notify_closed(&self, webview: WebView) {

ports/servoshell/webdriver.rs
@@ -14,7 +14,7 @@ use servo::{
 use url::Url;
 
 use crate::running_app_state::RunningAppState;
-use crate::window::PlatformWindow;
+use crate::window::{PlatformWindow, TopLevelWebViewCreationRequest};
 
 #[derive(Default)]
 pub(crate) struct WebDriverEmbedderControls {
@@ -159,7 +159,10 @@ impl RunningAppState {
                             NewWindowTypeHint::Window | NewWindowTypeHint::Auto,
                             Some(create_platform_window),
                         ) => {
-                            let window = self.open_window(create_platform_window(url.clone()), url);
+                            let window = self.open_window(
+                                create_platform_window(url.clone()),
+                                TopLevelWebViewCreationRequest::WithUrl(url),
+                            );
                             window
                                 .active_webview()
                                 .expect("Should have at last one WebView in new window")
@@ -169,7 +172,10 @@ impl RunningAppState {
                             .values()
                             .nth(0)
                             .expect("Expected at least one window to be open")
-                            .create_toplevel_webview(self.clone(), url),
+                            .create_toplevel_webview(
+                                self.clone(),
+                                TopLevelWebViewCreationRequest::WithUrl(url),
+                            ),
                     };
 
                     if let Err(error) = response_sender.send(new_webview.id()) {